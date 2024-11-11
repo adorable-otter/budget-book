f2011dd8ea8d4a8cfa58963b27485a0c37647292
adorable-otter <plan.stack@gmail.com>
2024-11-11 12:29:42 +0900

    라우터 적용, css reset, 기본 레이아웃 설정

---
+++ src/shared/Router.jsx
@@ -0,0 +1,26 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import styled from 'styled-components';
+import Home from '../pages/Home';
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
+  );
+};
+
+const Layout = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+  margin: 0;
+`;
+
+export default Router;
