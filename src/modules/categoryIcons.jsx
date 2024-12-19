0fad767d9357553780e6f35422429fafd672db07
adorable-otter <plan.stack@gmail.com>
2024-12-19 21:47:28 +0900

    카테고리 아이콘 추가 및 차트 색상 선택 함수 추가

--- src/modules/categoryIcons.jsx
+++ src/modules/categoryIcons.jsx
@@ -1,14 +1,28 @@
-import { FaBowlFood } from 'react-icons/fa6';
+import { FaBowlFood, FaHouseUser } from 'react-icons/fa6';
 import { FcAbout } from 'react-icons/fc';
+import { GiSatelliteCommunication } from 'react-icons/gi';
 import { MdEmojiTransportation } from 'react-icons/md';
 
 const getCategoryIcons = (name, { size }) => {
   const icons = {
-    none: <FcAbout size={size} />,
-    food: <FaBowlFood size={size} color="#88C273" />,
-    transportation: <MdEmojiTransportation size={size} color="#E07B39" />,
+    none: <FcAbout size={size} color={getIconColor('none')}/>,
+    food: <FaBowlFood size={size} color={getIconColor('food')} />,
+    transportation: <MdEmojiTransportation size={size} color={getIconColor('transportation')} />,
+    communication: <GiSatelliteCommunication size={size} color={getIconColor('communication')} />,
+    housing: <FaHouseUser size={size} color={getIconColor('housing')} />,
   };
   return icons[name];
+};
+
+export const getIconColor = (code) => {
+  const colors = {
+    none: '#0080ff',
+    food: '#88C273',
+    transportation: '#E07B39',
+    communication: '#AAABD3',
+    housing: '#ee6e9f',
+  };
+  return colors[code];
 };
 
 export default getCategoryIcons;
